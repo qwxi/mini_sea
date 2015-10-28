679b378eb5dc2de837192d98b751099c08865a2e
qwxi <876963639@qq.com>
2015-10-28 00:07:37 -0400

    modify rbtree-test.c

--- src/rbtree-test.c
+++ src/rbtree-test.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "rbtree.h"
 
 struct myfd{
     int fd;
-    struct myfd *fdptr;
+    struct myfd *next;
 };
 
-/*
-    unsigned long key;
-    struct myfd fdlist;   
-*/
+typedef time_t rb_key;
 
 struct mytype{
     struct rb_node node;
+    rb_key key;
+    struct myfd *list;
+/*
     char *keystring;
+*/
 };
 
 struct rb_root mytree = RB_ROOT;
 
-struct mytype *my_search(struct rb_root *root, char *string)
+struct mytype *my_search(struct rb_root *root, rb_key key)
 {
+        time_t  result;
         struct rb_node *node = root->rb_node;
 
         while (node) {
                 struct mytype *data = container_of(node, struct mytype, node);
-                int result;
 
-                result = strcmp(string, data->keystring);
+                result = key - data->key;
 
                 if (result < 0)
                         node = node->rb_left;
@@ -43,20 +45,34 @@ struct mytype *my_search(struct rb_root *root, char *string)
 
 int my_insert(struct rb_root *root, struct mytype *data)
 {
+        time_t result;
         struct rb_node **new = &(root->rb_node), *parent = NULL;
 
         /* Figure out where to put new node */
         while (*new) {
                 struct mytype *this = container_of(*new, struct mytype, node);
-                int result = strcmp(data->keystring, this->keystring);
+
+                result = data->key - this->key;
 
                 parent = *new;
                 if (result < 0)
                         new = &((*new)->rb_left);
                 else if (result > 0)
                         new = &((*new)->rb_right);
-                else
+                else{
+                        if(this->list == NULL)
+                        {
+                            this->list = data->list;                             
+                        }else{
+                            struct myfd *ptr = this->list; 
+                            while(ptr->next != NULL)
+                            {
+                                ptr = ptr->next;
+                            }
+                            ptr->next = data->list;
+                        }
                         return 0;
+                }
         }
 
         /* Add new node and rebalance tree. */
@@ -66,36 +82,75 @@ int my_insert(struct rb_root *root, struct mytype *data)
         return 1;
 }
 
-#define NUM 10
+#define NUM 100
 
 int main()
 {
 
+    struct timeval tv;
     struct mytype *mn[NUM];
+    struct mytype m[NUM];
+    struct myfd   f[NUM];
+
+
+    gettimeofday(&tv, NULL);
+    printf("%lu\n", tv.tv_sec);
 
     int i = 0;
     for(;i<NUM;i++)
     {
-        mn[i] = malloc(sizeof(struct mytype)) ;
-        mn[i]->keystring = malloc(sizeof(char)*4);
-        sprintf(mn[i]->keystring, "%d", i);
+        mn[i] = &(m[i]) ;
+
+        gettimeofday(&tv, NULL);
+
+        mn[i]->key = tv.tv_sec;
+
+        mn[i]->list = &(f[i]);
+
+        memset(mn[i]->list, 0, sizeof(struct myfd));
+        mn[i]->list->fd = i;
+        
+
         my_insert(&mytree, mn[i]);
     }
-
+ 
+    gettimeofday(&tv, NULL);
+    printf("%lu\n", tv.tv_sec);
+    printf("Start to Count...\n");
     struct rb_node *node;
     for(node=rb_first(&mytree);node;node=rb_next(node)) 
-          printf("key=[%s]\n", rb_entry(node, struct mytype, node)->keystring);
+    {
+    printf("key=[%lu]", rb_entry(node, struct mytype, node)->key);
+    printf("fdlist = [");
 
-    struct mytype *data = my_search(&mytree, "5");
+        struct myfd *ptr = rb_entry(node, struct mytype, node)->list;
+        if(ptr == NULL)
+        {
+           printf(" ");
+        }else{
+            while(ptr != NULL)
+            {
+                printf(" %d ", ptr->fd);
+                ptr = ptr->next;
+            }
+        }
+
+        printf(" ] \n");
+    }
+    gettimeofday(&tv, NULL);
+    printf("%lu\n", tv.tv_sec);
+
+    struct mytype *data = my_search(&mytree, tv.tv_sec);
     if(data)
     {
            rb_erase(&data->node, &mytree);
     }
 
-    printf("----------------\n");
+    printf("-------------------------------------\n");
 
     for(node=rb_first(&mytree);node;node=rb_next(node)) 
-          printf("key=[%s]\n", rb_entry(node, struct mytype, node)->keystring);
+          printf("key=[%lu]\n", rb_entry(node, struct mytype, node)->key);
+
 
     return 0;
 }
