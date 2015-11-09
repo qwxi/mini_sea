3b2bd7a68af8cf5d6154e5519dcbe86cf653194c
qwxi <876963639@qq.com>
2015-11-09 05:00:10 -0500

    log func ok

---
+++ src/mini_sea_log.c
@@ -0,0 +1,64 @@
+#include "mini_sea_log.h"
+
+int   mloginit(const char *logdir)
+{
+     pid = getpid();
+     logfilename = malloc(strlen(logdir)+64);
+     if(logfilename == NULL)
+     {
+         printf("malloc error [%s]\n", strerror(errno));
+         return -1;
+     }
+     sprintf(logfilename, "%s/%d", logdir, pid); 
+     return 0;
+}
+
+void  mlog(const char *fmt, ...)
+{
+     char log[LOGBUFLEN+1]  = {0}; 
+     char buf[LOGBUFLEN+EXTRLEN]  = {0}; 
+
+     va_list args;
+     va_start(args, fmt);
+     vsnprintf(log, LOGBUFLEN, fmt, args);
+     va_end(args);
+
+     struct timeval tv ;
+     gettimeofday(&tv, NULL);
+/*
+     time_t now;
+     struct tm *local = localtime(&now); 
+
+     sprintf(buf,"%04d-%02d-%02d %02d:%02d:%02d %s\n",  \
+                local->tm_year+1900,  \
+                local->tm_mon,        \
+                local->tm_mday,       \
+                local->tm_hour,       \
+                local->tm_min,        \
+                local->tm_sec, log); 
+*/
+     sprintf(buf, "%d:%d %d %s\n", tv.tv_sec, tv.tv_usec, pid, log);
+
+     int fd = open(logfilename, O_RDWR | O_CREAT | O_APPEND , S_IRWXU );  
+     write(fd, buf, LOGBUFLEN + EXTRLEN);
+     close(fd);
+
+     return ;
+}
+
+
+/*
+int main()
+{
+    
+    int ret =  mloginit("/home/wzx/Desktop");
+    int i ;
+  
+    for(i = 0; i < 10000; i++)
+    {
+        mlog("log func test %d", getpid);    
+    }
+
+    return 0;
+}
+*/
--- src/mini_sea_log.h
+++ src/mini_sea_log.h
@@ -1,7 +1,24 @@
 #ifndef _MINI_SEA_LOG_H_INCLUDE_
 #define _MINI_SEA_LOG_H_INCLUDE_
 
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+/*
+#include <time.h>
+*/
+
 #define LOGBUFLEN 1024
+#define EXTRLEN   32
 
 static char *logfilename;
 static pid_t pid;
